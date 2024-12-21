5a6a96bfde4c112c084ce27baa06f9073399ff84
GlxckADev <114743081+ShexYt@users.noreply.github.com>
2024-12-21 18:34:36 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>why are u here nigger</title>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="This script is free nigga, why u tryna skid!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          stop being  nigga <code>dawg</code>
         </p>
       </main>
 
