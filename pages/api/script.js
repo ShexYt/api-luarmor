394cad0156f2f79274b17d15e7c1e2531a0c522e
GlxckADev <114743081+ShexYt@users.noreply.github.com>
2024-12-21 18:57:11 +0800

    Update script.js

--- pages/api/script.js
+++ pages/api/script.js
@@ -1,12 +1,7 @@
 export default function handler(req, res) {
-  const secretKey = "FREE"; // Replace this with your secret key
-  const token = req.query.token; // Get token from URL
+  const luaCode = 'print("hi")'; // Lua script
+  const encodedCode = Buffer.from(luaCode).toString('base64'); // Base64 encode the Lua script
 
-  if (token !== secretKey) {
-    return res.status(403).send("Forbidden");
-  }
-
-  const encodedScript = Buffer.from('print("hi")').toString('base64');
   res.setHeader('Content-Type', 'text/plain');
-  res.status(200).send(`local decode = game.HttpService:JSONDecode; loadstring(decode([=[${encodedScript}]=]))()`);
+  res.status(200).send(`loadstring(game:GetService("HttpService"):Base64Decode("${encodedCode}"))()`);
 }
