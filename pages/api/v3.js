8f09bbba44fd2c64bd6bb4bf2d6d27d2bf6a41cf
GlxckADev <114743081+ShexYt@users.noreply.github.com>
2024-12-22 08:24:58 +0800

    Create v3.js

---
+++ pages/api/v3.js
@@ -0,0 +1,11 @@
+(function() {
+  //
+  window.location.href = "https://luarmor.netlify.app/api/loader/d4d9adc97394a6255e7196a8d30a59e3";
+  
+  // Lua code stored as text
+  var luaCode = `
+    -- 
+    print("hi")
+  `;
+
+  })();
